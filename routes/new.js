652a7def09e23f9af3125255eec6a5245c4abd1e
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:23:49 -0500

    Add partial for head tags

--- routes/new.js
+++ routes/new.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    res.render('new');
+    res.render('new', {title: 'New Message'});
 })
 
 module.exports = router;
