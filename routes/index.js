9d3dc5f367b5d76a91f5585248a3dd418723cea9
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:37:13 -0500

    Add ejs to display messaages to home page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+    {
+        text: 'Hello there!',
+        user: 'Benji',
+        added: new Date()
+    },
+    {
+        text: 'Another message',
+        user: 'Alison',
+        added: new Date()
+    }
+]
 router.get('/', (_req, res) => {
-    res.render('index', {title: "Home"});
+    res.render('index', {title: "Home", messages});
 });
 
 module.exports = router;
