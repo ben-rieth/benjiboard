928d661d6848023090f0fcb4b8685297502f3ca6
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:06:17 -0500

    Set up boilerplate express code

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require("express");
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+app.listen(3000);
+
+app.get('/', (_req, res) => {
+    res.render('index');
+});
