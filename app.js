3e588ff6514d7bfe10dd8453d379b926dab27f0e
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:39:55 -0500

    Add logger

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const express = require("express");
+const logger = require('morgan');
 
 const indexRoutes = require('./routes/index');
 const newRoutes = require('./routes/new')
 
 const app = express();
 
+//set template engine and view folder
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
+//set up logging using Morgan
+app.use(logger('dev'));
+
+//add routes to the app
 app.use('/', indexRoutes);
 app.use('/new', newRoutes);
 
