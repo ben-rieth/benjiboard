d5b898fe97381511b2501c4b604c5c342cd9cc7e
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:20:55 -0500

    Add routes for index and new

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require("express");
 
+const indexRoutes = require('./routes/index');
+const newRoutes = require('./routes/new')
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+app.use('/', indexRoutes);
+app.use('/new', newRoutes);
 
-app.get('/', (_req, res) => {
-    res.render('index');
-});
+app.listen('3000');
+
