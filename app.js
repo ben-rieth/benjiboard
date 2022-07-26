fef0ba401e3522e593ed56795350971bdf6d9f49
ben-rieth <benrieth3@gmail.com>
2022-07-26 18:56:23 -0500

    Add connection to database

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const dotenv = require('dotenv');
 const express = require("express");
 const logger = require('morgan');
 
 const indexRoutes = require('./routes/index');
 const newRoutes = require('./routes/new')
 
+dotenv.config();
 const app = express();
+
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@nodetest.nnpon.mongodb.net/message-board?retryWrites=true&w=majority`
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 //set template engine and view folder
 app.set('views', path.join(__dirname, 'views'))
